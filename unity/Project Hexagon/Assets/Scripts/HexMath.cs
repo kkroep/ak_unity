edf83343b52840916268a5c5cbadc386bc28b1d9
kkroep <k.kroep1@gmail.com>
2017-05-09 17:19:26 +0200

    hexDistance

--- unity/Project Hexagon/Assets/Scripts/HexMath.cs
+++ unity/Project Hexagon/Assets/Scripts/HexMath.cs
@@ -18,4 +18,8 @@ public class HexMath : MonoBehaviour
         else
             return (y + 0.5f) * (float)Math.Sqrt(3);
     }
+
+    public float hexDistance(float x1, float y1, float x2, float y2) {
+        return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
 }
