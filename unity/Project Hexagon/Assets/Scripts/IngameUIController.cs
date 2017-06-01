e95cf829c6749de2dedf486da7a4fbe3b330d00c
Alex Janssen <a.janssen@outlook.com>
2017-06-02 01:17:52 +0200

    IngameUI

---
+++ unity/Project Hexagon/Assets/Scripts/IngameUIController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameUIController : MonoBehaviour
+{
+
+    // Global variables
+    protected GameObject gameController;
+    protected Component boardController;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the gameController
+        gameController = GameObject.FindGameObjectWithTag("GameController");
+        boardController = gameController.GetComponent<BoardController>();
+
+        // Initiate the champions for the player on the positions that are wished
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void selectChampion(int championNumber)
+    {
+        if (championNumber == 1)
+        {
+
+        }
+
+        if (championNumber == 2)
+        {
+
+        }
+
+        if (championNumber == 3)
+        {
+
+        }
+    }
+}
