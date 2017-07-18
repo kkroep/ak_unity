32062ac4947d7439b88062fee70046fb64bf324e
Alex Janssen <a.janssen@outlook.com>
2017-07-18 22:08:07 +0200

    thesiskek

--- misc/harry_potter.m
+++ misc/harry_potter.m
@@ -1,4 +1,6 @@
-
+clc;
+close all;
+clear;
 
 colorspec = {[0.4 0 0.8]; [0.4 0.8 0]; [0.4 0.7 0.7]; ...
   [0 0.4 0.8]; [0.8 0.4 0]; [0.7 0.4 0.7]; ...
@@ -14,57 +16,78 @@ colorspec = {...
 [1.0 0 0.0]; ... 
 };
 
-graphics_toolkit gnuplot;
-figure ("visible", "off");
+%graphics_toolkit gnuplot;
+%figure ("visible", "off");
 
 
-PPS = 1e5; % [Hz]
-deadTimeSteps = 5; % multiple of stepSize
+PPS = [1e4 2e4 4e4]; % [Hz]
+deadTimeSteps = [400]; % multiple of stepSize
 
-endTime = 1e-2; % [s]
-stepSize = 1e-6; % [s]
+PPS = [5e4 5e5 5e6]; % [Hz]
+deadTimeSteps = [500]; % multiple of stepSize
+
+
+endTime = 2e-5; % [s]
+stepSize = 2e-9; % [s]
+
+
+%endTime = 2e-3; % [s]
+%stepSize = 1e-6; % [s]
+
 waitingTime = 0:stepSize:endTime;
-
-
-iterations = 1e4; % [-]
+ 
+iterations = 25e3; % [-]
 
 
 values = zeros(1,length(waitingTime));
 
-P_hit = 1-poisspdf(0, PPS*stepSize) % chance that one or more photons arrive during t_step
+legendString = {};
 
+hold on;
 
-for i=1:iterations
-    new_value = zeros(1,length(waitingTime));
-    current = 0 ;
-    dead = 0;
-    for t=1:length(waitingTime)
-        if rand<P_hit || dead<=0
-            current = mod(current+1,2);  % flip the current value
-            dead = deadTimeSteps;
+for pps = PPS
+    P_hit = 1-poisspdf(0, pps*stepSize) % chance that one or more photons arrive during t_stepT
+    for dTime = deadTimeSteps
+        for j=1:10
+            fprintf('%d0\n', j);
+            for i=1:iterations/10
+                new_value = zeros(1,length(waitingTime));
+                current = 0 ;
+                dead = 0;
+                for t=1:length(waitingTime)
+                    if dead>0
+                        dead = dead - 1;
+                    elseif rand<P_hit
+                        current = mod(current+1,2);  % flip the current value                
+                        dead = dTime;
+                    end
+                    new_value(t) = current;
+                    dead = dead - 1;
+                end
+                values = values + new_value;
+            end
         end
-        new_value(t) = current;
-        dead = dead-1;
+    values = values./iterations;
+    plot(waitingTime.*1e6, values);
+    legendString{end+1} = sprintf('PPS = %d', pps);
     end
-    values = values + new_value;
 end
 
-plot(waitingTime, values, 'Linewidth', 2, 'Color', colorspec{mod(1,6)+1});
-
-%hold off;
+plot(waitingTime*1e6, 0.5*ones(1,length(values)), 'k');
+hold off;
 
 %plot(C1(:,1),C1(:,4), 'Color', colorspec{mod(i,12)+1});
 %axis([C1(1,1) C1(end,1) min(min(C1))*1.1 max(max(C1))*1.1]);
-%xlim([min(C1(:,2)), max(C1(:,2))]);
+xlim([waitingTime(1)*1e6, waitingTime(end)*1e6]);
 %ylim([1e-1, 1e2]);
-%xlabel('packetrate [s]', 'fontsize', 14);
-%ylabel('RMSE [-]', 'fontsize', 14);
-%set(gca, 'FontSize', 12)
+xlabel('time [us]', 'fontsize', 14);
+ylabel('probability of measuring ''1''', 'fontsize', 14);
+set(gca, 'FontSize', 12)
 
 
-%legend(legendstring, 'Location', 'northeastoutside');
-%title('error to packetrate for varying delays. Packetsize 1', 'fontsize', 14);
+legend(legendString, 'Location', 'northeast');
+title(sprintf('deadtime = %d us', 1e6*stepSize*deadTimeSteps));
 %
-print('-dpdf', '-color', fullfile(pwd, 'sneep.pdf'));
+%print('-dpdf', '-color', fullfile(pwd, 'sneep.pdf'));
 %print('-deps', '-color', fullfile(pwd, 'lineplot.eps'));
 
