e0eff50b4904f45d46aa474fb4b73979bc19e705
Alex Janssen <a.janssen@outlook.com>
2017-07-24 22:33:57 +0200

    MATLAB2

---
+++ misc/MATLAB2/Untitled.m
@@ -0,0 +1,8 @@
+X = [0.001 0.005 0.01 0.05 0.1]
+Y = [0.05 0.35 0.5 0.81 0.91]
+
+plot(X,Y)
+
+title('physical error probability vs. logical bit flip errors')
+ylabel('Logical PE')
+xlabel('Physical PE')
